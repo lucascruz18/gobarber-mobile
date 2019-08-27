9950be16ff633a193ff8a33d3326b44029ebc2f3
Lucas <lucas@alternativainternet.com>
2019-08-27 14:45:43 -0300

    page of signup

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '../../assets/logo.png';
 
 import Background from '../../components/Background';
 
-export default function SignUp() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
   return (
     <Background>
-      <Text>Teste</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Criar conta</SubmitButton>
+
+          <SignLink onPress={() => navigation.navigate('SignIn')}>
+            <SignLinkText>Já possuo conta</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
     </Background>
   );
 }
