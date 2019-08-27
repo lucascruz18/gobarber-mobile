4c7866e27df81b26cacadd512bdea5fb1dde329a
Lucas <lucas@alternativainternet.com>
2019-08-27 14:38:35 -0300

    first commit

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import logo from '../../assets/logo.png';
+
+import Background from '../../components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignIn() {
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+
+          <SignLink onPress={() => {}}>
+            <SignLinkText>Criar conta gratuita</SignLinkText>
+          </SignLink>
+        </Form>
+      </Container>
+    </Background>
+  );
+}
